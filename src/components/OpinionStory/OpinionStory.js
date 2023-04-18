4c2f5da2f7fc42207402f59c138c6f96ada6bcaf
Abi Tyas Tunggal <abi@tyastunggal.com>
2023-04-18 16:22:34 +1000

    Finish main story grid

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -18,6 +18,10 @@ const OpinionStory = ({ id, title, author, avatar }) => {
 
 const Wrapper = styled.article`
   color: var(--color-gray-900);
+
+  @media ${QUERIES.tabletOnly} {
+    border-bottom: none;
+  }
 `;
 
 const Avatar = styled.img`

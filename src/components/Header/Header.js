d8ce47bd569d4ed38b6d2578e17af4a4277cefda
Abi Tyas Tunggal <abi@tyastunggal.com>
2023-04-18 11:44:04 +1000

    Finish header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,20 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActions>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+        </DesktopActions>
         <Logo />
+        
+        <SubscribeGroup>
+          <Button>Subscribe</Button>  
+          <SubLink href="">Already a subscriber?</SubLink>
+        </SubscribeGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +52,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +76,56 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopActions = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    margin-top: 16px;
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    justify-content: start;
+  }
+`;
+
+const SubscribeGroup = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+    position: relative;
+    justify-self: end;
+    
+  }
+`;
+
+const SubLink = styled.a`
+  position: absolute;
+  width: 100%;
+  margin-top: 8px;
+  text-align: center;
+  color: var(--color-gray-900); 
+  text-decoration: underline;
+  font-size: calc(14 / 16)rem;
+  font-style: italic;
+  font-weight: 400;
 `;
 
 export default Header;
